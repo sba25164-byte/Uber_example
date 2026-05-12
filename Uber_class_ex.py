6779e75d3375f989c2d85d691d98cf2d80b05279
sba25164-byte <sba25164@student.cct.ie>
2026-05-12 18:53:16 +0100

    update3

--- Uber_class_ex.py
+++ Uber_class_ex.py
@@ -14,3 +14,7 @@ def load_data(nrows):
 	data.rename(lowercase,axis="columns",inplace=True)
 	data[DATE_COLUMN]=pd.to_datetime(data[DATE_COLUMN])
 	return data
+
+data_load_state=st.text("Loading Data...")
+data=load_data(100000)
+data_load_state.text("Loading Data Done")
