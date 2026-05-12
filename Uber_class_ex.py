748daf87caffceb7242eff59b6005028498a9009
sba25164-byte <sba25164@student.cct.ie>
2026-05-12 19:17:44 +0100

    update7

--- Uber_class_ex.py
+++ Uber_class_ex.py
@@ -26,5 +26,10 @@ st.subheader("Num Pickups per Hour")
 hist_values=np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
 
-st.subheader("Map of Pickups")
+st.subheader("Map of Pickups - not filterable/ useful")
 st.map(data)
+
+hour_to_filter=st.slider("hour", 0,23,17)
+filtered_data=data[data[DATE_COLUMN].dt.hour==hour_to_filter]
+st.subheader(f"Map of all Pickups at{hour_to_filter}:00")
+st.map(filtered_data)
